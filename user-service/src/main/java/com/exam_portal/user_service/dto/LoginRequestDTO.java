ae32126564207a2f6c4ab1bf417520d9740de001
K Y <2407106@cognizant.com>
2025-06-23 17:19:47 +0530

    user service

---
+++ user-service/src/main/java/com/exam_portal/user_service/dto/LoginRequestDTO.java
@@ -0,0 +1,13 @@
+package com.exam_portal.user_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDTO {
+    private String email;
+    private String password;
+}
---
+++ user-service/src/main/java/com/exam_portal/user_service/service/UserService.java
@@ -0,0 +1,13 @@
+package com.exam_portal.user_service.service;
+
+import com.exam_portal.user_service.dto.UserDTO;
+
+public interface UserService {
+    UserDTO registerUser(UserDTO userDTO);
+    String loginUser(String email, String password);
+    UserDTO getUserProfileFromToken(String token);
+    UserDTO updateUserProfile(String token, UserDTO updatedUser);
+    UserDTO getUserByEmail(String email);
+    UserDTO getUserById(Long id);
+}
+
