25ed973ef528103d66b7c12477913ffb518b2521
Satvikshegde29 <Satvik.Hegde@cognizant.com>
2025-06-26 12:41:18 +0530

    making config repo

--- user-service/src/test/java/com/exam_portal/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/exam_portal/user_service/UserServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.exam_portal.user_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.config.import=optional:configserver:http://localhost:8888"
+})
 class UserServiceApplicationTests {
 
 	@Test
