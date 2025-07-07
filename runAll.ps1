af225efb662297c801686fb78ec3ffc49211924c
Satvikshegde29 <Satvik.Hegde@cognizant.com>
2025-07-07 11:36:48 +0530

    api gateway is added

--- runAll.ps1
+++
@@ -1,3 +0,0 @@
-
-Write-Host "Starting Discovery Service..."
-& 'C:\Program Files\Java\jdk-21\bin\java.exe' '@C:\Users\2407093\AppData\Local\Temp\cp_discovery.argfile' 'com.examportal.discovery.DiscoveryServiceApplication'
